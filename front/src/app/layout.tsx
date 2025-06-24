a84baa6360d78c7e0e198a467bfa1245f28d8627
Baptiste <baptiste.faisy@gmail.com>
2025-06-24 22:42:02 +0200

    react

---
+++ front/src/app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Jeu de Combat',
+  description: 'Personnalisez votre partie de jeu',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="fr">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
