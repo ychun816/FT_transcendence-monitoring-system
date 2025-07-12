991f79b1d9def18e5acdb8095a86346a9d2d7c9b
HADMARINE <joonlee031@live.com>
2025-07-12 12:25:41 +0200

    front minor

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,15 +1,15 @@
 "use client";
 
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-import styles from './HomePage.module.css'; // Optionnel si vous voulez ajouter du style
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import styles from "./HomePage.module.css"; // Optionnel si vous voulez ajouter du style
 
 export default function HomePage() {
   const router = useRouter();
 
   useEffect(() => {
     // Rediriger immédiatement vers la page de login
-    router.push('/login');
+    router.replace("/login");
   }, []);
 
   return (
